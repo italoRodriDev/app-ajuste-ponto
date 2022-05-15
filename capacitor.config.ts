a0b5a04d6ac4bdbe31be560dc11aa5a18f825247
Italo Rodrigues <irodrigues@3xsolutions.com.br>
2022-05-15 10:32:51 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ajuste de Ponto',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
